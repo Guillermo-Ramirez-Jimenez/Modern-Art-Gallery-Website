0fdeeb1e7bdd9cd6d836d0d024834e12a1cee49e
Chache <guillermo.ramirez.jimenez.0@gmail.com>
2025-07-15 00:08:37 +0200

    Added rollupOptions to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: "https://guillermo-ramirez-jimenez.github.io/Modern-Art-Gallery-Website/", // Usa rutas relativas en los enlaces generados en el HTML
+    base: "/Modern-Art-Gallery-Website/", // Usa rutas relativas en los enlaces generados en el HTML
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                location: resolve(__dirname, 'location.html')
+            }
+        }
+    }
 });
